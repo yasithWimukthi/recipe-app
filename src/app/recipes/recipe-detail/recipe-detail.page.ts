ecd4bc1b18dd78accf57e7c6b78e24ca3c87420d
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-23 19:58:14 +0530

    Implement recipe detail page

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RecipesService} from "../recipes.service";
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailPage implements OnInit {
 
-  constructor() { }
+  loadedRecipe : Recipe;
+
+  constructor(
+    private activatedRoute:ActivatedRoute,
+    private recipeService:RecipesService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap =>{
+      if(!paramMap.has('recipeId')){
+        return;
+      }
+      const recipeID = paramMap.get('recipeId');
+      this.loadedRecipe = this.recipeService.getRecipe(recipeID);
+    })
   }
 
 }
